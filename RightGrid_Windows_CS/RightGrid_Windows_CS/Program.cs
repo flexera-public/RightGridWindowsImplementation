56de52942c1521b9c2ad0dee92b7e66ae9d5657d
Richard Shade <rshade@rightscale.com>
2011-12-30 16:15:28 -0600

    commit after OO

--- RightGrid_Windows_CS/RightGrid_Windows_CS/Program.cs
+++ RightGrid_Windows_CS/RightGrid_Windows_CS/Program.cs
@@ -17,7 +17,6 @@ using Amazon.SQS.Model;
 using Yaml;
 
 
-
 // Yaml Parser - http://www.codeproject.com/KB/recipes/yamlparser.aspx
 // AWS.NetSDK - http://aws.amazon.com/sdkfornet/
 //http://sufianrashid.wordpress.com/2011/10/17/c-modify-app-config-file-at-run-time/
@@ -28,151 +27,16 @@ namespace WinRightGrid
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine(ConfigurationManager.AppSettings["AWSAccessKey"]);
             Console.WriteLine(Queue.ListSQSQueues());
-            Console.WriteLine(AddSQSMessage());
-            Console.Write(GetServiceOutput());
+            Console.WriteLine(Queue.Send("test"));
             Console.Read();
         }
-
-        public static string AddSQSMessage()
+        public static void Run()
         {
-            AmazonSQS sqs = AWSClientFactory.CreateAmazonSQSClient();
-            SendMessageRequest msgreq = new SendMessageRequest();
-            msgreq.QueueUrl = ConfigurationManager.AppSettings["SQSUrl"];
-            string yamltest = @"Key1:
-                              A: Item A
-                              B: Item B
-                            Key2:
-                              A: Item A
-                              B: Item B
-                            ";
-            Sequence sequence = new Sequence (
-                new Node [] {
-                    new Yaml.String("rightworker"),
-                    new Mapping (
-                        new MappingNode []
-                        {
-                            new MappingNode(new Yaml.String("sqs_url"), new Yaml.String(ConfigurationManager.AppSettings["SQSUrl"].ToString())),
-                            new MappingNode(new Yaml.String("s3_bucket"),new Yaml.String("rs-test"))
-                        }
-                        )
-                }
-                );
-            Console.WriteLine(sequence);
-            msgreq.MessageBody = sequence.ToString();
-            SendMessageResponse msgres = sqs.SendMessage(msgreq);
-            SendMessageResult msgrst = msgres.SendMessageResult;
-            return msgrst.ToString();
-        }
-        public static string GetServiceOutput()
-        {
-            StringBuilder sb = new StringBuilder(1024);
-            using (StringWriter sr = new StringWriter(sb))
+            while (Queue.Count() > 1)
             {
-                sr.WriteLine("===========================================");
-                sr.WriteLine("Welcome to the AWS .NET SDK!");
-                sr.WriteLine("===========================================");
-
-                // Print the number of Amazon EC2 instances.
-                AmazonEC2 ec2 = AWSClientFactory.CreateAmazonEC2Client();
-                DescribeInstancesRequest ec2Request = new DescribeInstancesRequest();
-
-                try
-                {
-                    DescribeInstancesResponse ec2Response = ec2.DescribeInstances(ec2Request);
-                    int numInstances = 0;
-                    numInstances = ec2Response.DescribeInstancesResult.Reservation.Count;
-                    sr.WriteLine("You have " + numInstances + " Amazon EC2 instance(s) running in the US-East (Northern Virginia) region.");
-
-                }
-                catch (AmazonEC2Exception ex)
-                {
-                    if (ex.ErrorCode != null && ex.ErrorCode.Equals("AuthFailure"))
-                    {
-                        sr.WriteLine("The account you are using is not signed up for Amazon EC2.");
-                        sr.WriteLine("You can sign up for Amazon EC2 at http://aws.amazon.com/ec2");
-                    }
-                    else
-                    {
-                        sr.WriteLine("Caught Exception: " + ex.Message);
-                        sr.WriteLine("Response Status Code: " + ex.StatusCode);
-                        sr.WriteLine("Error Code: " + ex.ErrorCode);
-                        sr.WriteLine("Error Type: " + ex.ErrorType);
-                        sr.WriteLine("Request ID: " + ex.RequestId);
-                        sr.WriteLine("XML: " + ex.XML);
-                    }
-                }
-                sr.WriteLine();
-
-                // Print the number of Amazon SimpleDB domains.
-                AmazonSimpleDB sdb = AWSClientFactory.CreateAmazonSimpleDBClient();
-                ListDomainsRequest sdbRequest = new ListDomainsRequest();
-
-                try
-                {
-                    ListDomainsResponse sdbResponse = sdb.ListDomains(sdbRequest);
-
-                    if (sdbResponse.IsSetListDomainsResult())
-                    {
-                        int numDomains = 0;
-                        numDomains = sdbResponse.ListDomainsResult.DomainName.Count;
-                        sr.WriteLine("You have " + numDomains + " Amazon SimpleDB domain(s) in the US-East (Northern Virginia) region.");
-                    }
-                }
-                catch (AmazonSimpleDBException ex)
-                {
-                    if (ex.ErrorCode != null && ex.ErrorCode.Equals("AuthFailure"))
-                    {
-                        sr.WriteLine("The account you are using is not signed up for Amazon SimpleDB.");
-                        sr.WriteLine("You can sign up for Amazon SimpleDB at http://aws.amazon.com/simpledb");
-                    }
-                    else
-                    {
-                        sr.WriteLine("Caught Exception: " + ex.Message);
-                        sr.WriteLine("Response Status Code: " + ex.StatusCode);
-                        sr.WriteLine("Error Code: " + ex.ErrorCode);
-                        sr.WriteLine("Error Type: " + ex.ErrorType);
-                        sr.WriteLine("Request ID: " + ex.RequestId);
-                        sr.WriteLine("XML: " + ex.XML);
-                    }
-                }
-                sr.WriteLine();
-
-                // Print the number of Amazon S3 Buckets.
-                AmazonS3 s3Client = AWSClientFactory.CreateAmazonS3Client();
-
-                try
-                {
-                    ListBucketsResponse response = s3Client.ListBuckets();
-                    int numBuckets = 0;
-                    if (response.Buckets != null &&
-                        response.Buckets.Count > 0)
-                    {
-                        numBuckets = response.Buckets.Count;
-                    }
-                    sr.WriteLine("You have " + numBuckets + " Amazon S3 bucket(s) in the US Standard region.");
-                }
-                catch (AmazonS3Exception ex)
-                {
-                    if (ex.ErrorCode != null && (ex.ErrorCode.Equals("InvalidAccessKeyId") ||
-                        ex.ErrorCode.Equals("InvalidSecurity")))
-                    {
-                        sr.WriteLine("Please check the provided AWS Credentials.");
-                        sr.WriteLine("If you haven't signed up for Amazon S3, please visit http://aws.amazon.com/s3");
-                    }
-                    else
-                    {
-                        sr.WriteLine("Caught Exception: " + ex.Message);
-                        sr.WriteLine("Response Status Code: " + ex.StatusCode);
-                        sr.WriteLine("Error Code: " + ex.ErrorCode);
-                        sr.WriteLine("Request ID: " + ex.RequestId);
-                        sr.WriteLine("XML: " + ex.XML);
-                    }
-                }
-                sr.WriteLine("Press any key to continue...");
+                Console.WriteLine("test");
             }
-            return sb.ToString();
         }
     }
 }
--- RightGrid_Windows_CS/RightGrid_Windows_CS/Queue.cs
+++ RightGrid_Windows_CS/RightGrid_Windows_CS/Queue.cs
@@ -2,15 +2,25 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Configuration;
+//Amazon Libraries
 using Amazon;
 using Amazon.SQS;
 using Amazon.SQS.Model;
 
-namespace AWS_Console_App1
+namespace WinRightGrid
 {
     class Queue
     {
-        public static void Send(string msg) { }
+        public static string Send(string msg) {
+            AmazonSQS sqs = AWSClientFactory.CreateAmazonSQSClient();
+            SendMessageRequest msgreq = new SendMessageRequest();
+            msgreq.QueueUrl = ConfigurationManager.AppSettings["SQSUrl"];
+            msgreq.MessageBody = msg;
+            SendMessageResponse msgres = sqs.SendMessage(msgreq);
+            SendMessageResult msgrst = msgres.SendMessageResult;
+            return msgrst.ToString();
+        }
         public static string Get() { return "msg"; }
         public static int Count() { return 1; }
         public static string ListSQSQueues()
