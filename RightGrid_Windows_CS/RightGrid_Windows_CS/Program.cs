afaea7735611d55c6ca259af8a0c5681d1eaf6c4
Richard Shade <rshade@rightscale.com>
2012-01-03 15:45:49 -0600

    moved functions to queuetest

--- RightGrid_Windows_CS/RightGrid_Windows_CS/Program.cs
+++ RightGrid_Windows_CS/RightGrid_Windows_CS/Program.cs
@@ -22,19 +22,7 @@ namespace WinRightGrid
     {
       public static void Main(string[] args)
         {
-            Console.WriteLine(Queue.ListSQSQueues());
-            Console.WriteLine(Queue.Count());
-            Console.WriteLine("sending Message");
-            //Console.WriteLine(Queue.Send("test"));
-            Console.WriteLine("Getting Message");
-            Message msg = Queue.Get();
-            Console.WriteLine(msg.ReceiptHandle.ToString());
-            Console.WriteLine(msg.Body.ToString());
-            Console.WriteLine("New Count");
-            Console.WriteLine(Queue.Count());
-            Console.WriteLine("Delete Message");
-            Console.WriteLine(Queue.Delete(msg.ReceiptHandle.ToString()));
-            Queue.Count();
+            if (ConfigurationManager.AppSettings["QueueTest"] == "1") { QueueTest(); }; 
             Console.Read();
         }
         public static void Run()
@@ -43,6 +31,25 @@ namespace WinRightGrid
             {
                 Console.WriteLine("test");
             }
+        }
+        public static void QueueTest() {
+
+            Console.WriteLine(Queue.ListSQSQueues());
+            Console.WriteLine(Queue.Count());
+            Console.WriteLine("sending Message");
+            Console.WriteLine(Queue.Send("test"));
+            Console.WriteLine("Getting Message");
+            Message msg = Queue.Get();
+            if (msg != null)
+            {
+                Console.WriteLine(msg.ReceiptHandle.ToString() ?? null);
+                Console.WriteLine(msg.Body.ToString() ?? null);
+                Console.WriteLine("New Count");
+                Console.WriteLine(Queue.Count());
+                Console.WriteLine("Delete Message");
+                Console.WriteLine(Queue.Delete(msg.ReceiptHandle.ToString()));
+            }
+            Queue.Count();
         }
     }
 }
