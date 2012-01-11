ae817b26d670bc9137c017e08d03981c85008c63
Richard Shade <rshade@rightscale.com>
2012-01-11 11:34:32 -0600

    YamlDotNet Full Bin

--- RightGrid_Windows_CS/RightGrid_Windows_CS/Program.cs
+++ RightGrid_Windows_CS/RightGrid_Windows_CS/Program.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Configuration;
+using System.Management;
 
 using Amazon;
 using Amazon.EC2;
@@ -14,7 +15,10 @@ using Amazon.S3;
 using Amazon.S3.Model;
 using Amazon.SQS;
 using Amazon.SQS.Model;
-using Yaml;
+using YamlDotNet;
+using YamlDotNet.Core;
+using YamlDotNet.RepresentationModel;
+using YamlDotNet.Converters;
 
 namespace WinRightGrid
 {
@@ -36,15 +40,26 @@ namespace WinRightGrid
                 Message msg = Queue.Get(ConfigurationManager.AppSettings["input_queue_url"]);
                 Console.WriteLine("got message");
                 Console.WriteLine(msg.Body);
-                Node node = Node.Parse(msg.Body);
+                //Node node = Node.Parse(msg.Body);
                 Console.WriteLine("parsed message");
+                Stream input = new MemoryStream(Encoding.UTF8.GetBytes(msg.Body));
+                TextReader yamltxt = new StreamReader(input);
+                YamlStream yaml = new YamlStream();
+                yaml.Load(yamltxt);
+                YamlMappingNode mapping = (YamlMappingNode)yaml.Documents[0].RootNode;
+                foreach (KeyValuePair<YamlNode, YamlNode> entry in mapping.Children)
+                {
+                    Console.WriteLine(((YamlScalarNode)entry.Key).Value);
+                }
+                 
                 //Storage.Get(ConfigurationManager.AppSettings["S3_Bucket"], ConfigurationManager.AppSettings["S3_Input_Path"], node["input_file"]);
                 //Runner.run(node["input_file"],node["output_file"]));
                 //Storage.Put(ConfigurationManager.AppSettings["S3_Bucket"], ConfigurationManager.AppSettings["S3_Output_Path"], node["output_file"]);
-                Console.WriteLine(node);
+                Console.WriteLine("node");
                 Queue.Delete(ConfigurationManager.AppSettings["input_queue_url"], msg.ReceiptHandle.ToString());
             }
         }
+
         
     }
 }
